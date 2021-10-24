1233286daf82ecf379c9fb6799c9a2844b52453a
Marcos <marcosfuentesaguilera@gmail.com>
2021-10-24 12:51:02 +0200

    Created 2 type of tiles and generating them in a random order

--- Board game/Assets/Scripts/GridManager.cs
+++ Board game/Assets/Scripts/GridManager.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
 
-    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Tile _grassTile, _mountainTile;
 
     [SerializeField] private Transform _cam;
 
@@ -18,23 +19,24 @@ public class GridManager : MonoBehaviour
         GenerateGrid();
     }
     
-    void GenerateGrid() {
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, Tile>();
         for (int x = 0; x < _width; x++) {
             for (int y = 0; y < _height; y++) {
-                var spandedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
-                spandedTile.name = $"Tile {x} {y}";
+                var randomTile = Random.Range(0, 6) == 3 ? _mountainTile : _grassTile;
+                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
 
-                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                spandedTile.Init(isOffset);
-                _tiles[new Vector2(x, y)] = spandedTile;
+                spawnedTile.Init(x,y);
+                _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
 
         _cam.transform.position = new Vector3((float) _width / 2 - 0.5f, (float) _height / 2 - 0.5f, -30);
     }
 
-    public Tile GetTileAtPosition(Vector2 pos)
-    {
+    public Tile GetTileAtPosition(Vector2 pos) {
         return _tiles.TryGetValue(pos, out var tile) ? tile : null;   
     }
 }
