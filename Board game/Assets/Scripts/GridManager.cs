e39776b724f8adaed992321faaa12118f0498149
Marcos <marcosfuentesaguilera@gmail.com>
2021-10-24 10:57:52 +0200

    Added the creation of a simple board

--- Board game/Assets/Scripts/GridManager.cs
+++ Board game/Assets/Scripts/GridManager.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    [SerializeField] private Transform _cam;
+
+    private Dictionary<Vector2, Tile> _tiles;
+
+    private void Start()
     {
-        
+        GenerateGrid();
+    }
+    
+    void GenerateGrid() {
+        for (int x = 0; x < _width; x++) {
+            for (int y = 0; y < _height; y++) {
+                var spandedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spandedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spandedTile.Init(isOffset);
+                _tiles[new Vector2(x, y)] = spandedTile;
+            }
+        }
+
+        _cam.transform.position = new Vector3((float) _width / 2 - 0.5f, (float) _height / 2 - 0.5f, -30);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Tile GetTileAtPosition(Vector2 pos)
     {
-        
+        return _tiles.TryGetValue(pos, out var tile) ? tile : null;   
     }
 }
+
